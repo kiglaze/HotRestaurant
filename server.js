9e31869e7cf53420f4d21b3f22da9d29441aee0c
Vaijayanthi Neelakandan <nvaij@yahoo.com>
2017-09-20 21:17:15 -0400

    updated server js

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
+// Basic route that sends the user first to the AJAX Page
+app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
 app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
